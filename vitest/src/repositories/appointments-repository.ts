6b9e08fb3509e485da003631e4c0f018dc242ca3
guhrodriguess <gumartinsR1201@gmail.com>
2023-10-16 20:49:20 -0300

    API in Node.js with Vitest

---
+++ vitest/src/repositories/appointments-repository.ts
@@ -0,0 +1,9 @@
+import { Appointment } from "../entities/appointment";
+
+export interface AppointmentsRepository {
+  create(appointment: Appointment): Promise<void>;
+  findOverlappingAppointment(
+    startsAt: Date,
+    endsDate: Date
+  ): Promise<Appointment | null>;
+}
